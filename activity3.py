7fcfcf4f5252e73a65e50e83618241f24ae64691
madhuksharas35176-blip <madhuksharas_35176@chettinadvidyashram.org>
2026-03-30 17:39:04 +0530

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,11 @@
+print("enter the marks obtained in 4 subjests:")
+math=int(input("math:"))
+english=int(input("english:"))
+science= int(input("science:")) 
+hindi= int(input("hindi:"))   
+sum = math + english + science + hindi
+print("sum of math,english,science=hindi =",sum)   
+perc = (sum/400*100)        
+print(end = "percentage mark = ")   
+print(perc)        
+               
